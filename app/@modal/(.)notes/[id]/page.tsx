20d4b8f76166a57460a4d6aca99cc908b53785e5
Yuri <davydiuk.yurii@gmail.com>
2026-04-26 14:27:10 +0300

    add interface to [id]/page.tsx

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,5 +1,9 @@
 import NotePreview from './NotePreview.client';
 
-export default async function Page({ params }) {
+interface Props {
+  params: Promise<{ id: string }>;
+}
+
+export default async function Page({ params }: Props) {
   return <NotePreview params={params} />;
 }
